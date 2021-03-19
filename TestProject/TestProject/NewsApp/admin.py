2c90ae05bd28ce6a17cf8def3416cb362399f98d
Paul Thomas <paulthomas2107@gmail.com>
2021-03-19 15:20:13 +0000

    Added publication / articles

--- TestProject/TestProject/NewsApp/admin.py
+++ TestProject/TestProject/NewsApp/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import News, SportsNews, RegistrationData
+from .models import News, SportsNews, RegistrationData, Publication, Article
 
 # Register your models here.
 admin.site.register(News)
 admin.site.register(SportsNews)
 admin.site.register(RegistrationData)
+admin.site.register(Publication)
+admin.site.register(Article)
+
--- TestProject/TestProject/NewsApp/models.py
+++ TestProject/TestProject/NewsApp/models.py
@@ -4,6 +4,27 @@ from django.utils import timezone
 # Create your models here.
 
 
+class Publication(models.Model):
+    title = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('title',)
+
+
+class Article(models.Model):
+    headline = models.CharField(max_length=300)
+    publications = models.ManyToManyField(Publication)
+
+    def __str__(self):
+        return self.headline
+
+    class Meta:
+        ordering = ('headline',)
+
+
 class News(models.Model):
     author = models.CharField(max_length=30)
     title = models.CharField(max_length=30)
