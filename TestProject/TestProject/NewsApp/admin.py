30edb5b4f7f07df3dd34bd3a9b16b64d3d439aa3
Paul Thomas <paulthomas2107@gmail.com>
2021-03-19 16:48:14 +0000

    Added place and Restaurant

--- TestProject/TestProject/NewsApp/admin.py
+++ TestProject/TestProject/NewsApp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import News, SportsNews, RegistrationData, Publication, Article, Article2, Reporter
+from .models import News, SportsNews, RegistrationData, Publication, Article, Article2, Reporter, Place, Restaurant
 
 # Register your models here.
 admin.site.register(News)
@@ -9,5 +9,7 @@ admin.site.register(Publication)
 admin.site.register(Article)
 admin.site.register(Article2)
 admin.site.register(Reporter)
+admin.site.register(Place)
+admin.site.register(Restaurant)
 
 
--- TestProject/TestProject/NewsApp/models.py
+++ TestProject/TestProject/NewsApp/models.py
@@ -4,6 +4,23 @@ from django.utils import timezone
 # Create your models here.
 
 
+class Place(models.Model):
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Restaurant(models.Model):
+    place = models.OneToOneField(Place, on_delete=models.CASCADE, primary_key=True)
+    serves_hot_dogs = models.BooleanField(default=False)
+    serves_pizza = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.place.name
+
+
 class Reporter(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
